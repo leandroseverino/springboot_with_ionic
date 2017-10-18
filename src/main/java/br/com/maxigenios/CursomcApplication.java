1aae2c29ec1364fdbc8561ee7b9e6206a5e5510d
leandroseverino <lseverino@gmail.com>
2017-10-18 09:49:33 -0200

    Add load category when application starts

--- src/main/java/br/com/maxigenios/CursomcApplication.java
+++ src/main/java/br/com/maxigenios/CursomcApplication.java
@@ -1,12 +1,32 @@
 package br.com.maxigenios;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.maxigenios.domain.Categoria;
+import br.com.maxigenios.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repository.save(Arrays.asList(cat1, cat2));
+		
 	}
 }
--- src/main/java/br/com/maxigenios/resources/CategoriaResource.java
+++ src/main/java/br/com/maxigenios/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package br.com.maxigenios.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.maxigenios.domain.Categoria;
 import br.com.maxigenios.services.CategoriaService;
 
 @RestController
