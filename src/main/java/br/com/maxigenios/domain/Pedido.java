8a283995ba1bb670ea5d6d2ab5ee94056240683d
leandroseverino <lseverino@gmail.com>
2017-10-27 11:22:36 -0200

    Resolve problem when save a Pedido

--- src/main/java/br/com/maxigenios/domain/Pedido.java
+++ src/main/java/br/com/maxigenios/domain/Pedido.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 /**
  * @author Leandro Severino - https://about.me/leandroseverino
@@ -37,11 +36,9 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern="dd/MM/yyyy hh:mm")
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private Cliente cliente;
