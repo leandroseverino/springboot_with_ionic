9a47259e83bd898ba61e9516cfc9bc222b1b03bf
leandroseverino <lseverino@gmail.com>
2017-10-29 17:45:47 -0200

    Add POST endpoints rules permission

--- src/main/java/br/com/maxigenios/config/SecurityConfig.java
+++ src/main/java/br/com/maxigenios/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +24,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -40,9 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] READONLY_MATCHERS = {
 			"/produto/**",
-			"/categoria/**",
-			"/cliente/**"
+			"/categoria/**"
 	};
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/cliente/**"
+    };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -54,9 +59,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, READONLY_MATCHERS).permitAll()
-			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			.anyRequest().authenticated();
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			    .antMatchers(HttpMethod.GET, READONLY_MATCHERS).permitAll()
+			    .antMatchers(PUBLIC_MATCHERS).permitAll()
+			    .anyRequest().authenticated();
 
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
