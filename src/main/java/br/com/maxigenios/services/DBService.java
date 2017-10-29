ac3e0e7fb522edc3250e44b54eff218d2af247d4
leandroseverino <lseverino@gmail.com>
2017-10-28 23:35:08 -0200

    Add SmtpEmailService to sent an email of order confirmation !

--- src/main/java/br/com/maxigenios/services/DBService.java
+++ src/main/java/br/com/maxigenios/services/DBService.java
@@ -110,7 +110,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "99999999999", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "leandro@professionalit.com.br", "99999999999", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("273633323", "98383399"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
