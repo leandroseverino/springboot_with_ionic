465eb8c67cc96f1a016692aaa2a581328226b381
leandroseverino <lseverino@gmail.com>
2017-10-29 18:31:43 -0200

    Add refresh token endpoint

---
+++ src/main/java/br/com/maxigenios/resources/AuthResource.java
@@ -0,0 +1,38 @@
+package br.com.maxigenios.resources;
+
+import br.com.maxigenios.domain.Categoria;
+import br.com.maxigenios.dto.CategoriaDTO;
+import br.com.maxigenios.security.JWTUtil;
+import br.com.maxigenios.security.UserSS;
+import br.com.maxigenios.services.CategoriaService;
+import br.com.maxigenios.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
