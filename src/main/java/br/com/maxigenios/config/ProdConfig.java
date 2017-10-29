861602b9d703e6dec79f2372515d1eb6f8818089
leandroseverino <lseverino@gmail.com>
2017-10-29 00:06:57 -0200

    Initial implementation of Security Config to endpoints

--- src/main/java/br/com/maxigenios/config/ProdConfig.java
+++ src/main/java/br/com/maxigenios/config/ProdConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.maxigenios.services.DBService;
+import br.com.maxigenios.services.EmailService;
+import br.com.maxigenios.services.SmtpEmailService;
 
 @Configuration
 @Profile("prod")
@@ -28,5 +30,10 @@ public class ProdConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
